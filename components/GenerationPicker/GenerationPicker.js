e22e5ccf6200888cd2b7dd67fa37c2f67ab834f3
Henrik Wilhelm Sissener <hws902@gmail.com>
2020-12-07 23:32:23 +0100

    Added some simple animations

--- components/GenerationPicker/GenerationPicker.js
+++ components/GenerationPicker/GenerationPicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import Styles from "./styles";
@@ -6,16 +6,39 @@ import FakeLink from "../FakeLink/FakeLink";
 import { useRouter } from "next/router";
 import { Block, Flex, Grid, GridItem, InlineBlock } from "../../primitives";
 import Button from "../Button/Button";
+import { animated, useSpring } from "react-spring";
 
 const GenerationPicker = () => {
-  const router = useRouter();
-  const { route = "" } = router;
+  const x = 0;
+  const y = 0;
+  const rotation = 0;
+  const scale = 1.3;
+  const timing = 150;
 
+  const [isBooped, setIsBooped] = useState(false);
+
+  const style = useSpring({
+    display: "inline-block",
+    transform: isBooped
+      ? `translate(${x}px, ${y}px)
+         rotate(${rotation}deg)
+         scale(${scale})`
+      : `translate(0px, 0px)
+         rotate(0deg)
+         scale(1)`,
+    config: {
+      tension: 300,
+      friction: 10,
+    },
+  });
+  console.log(isBooped);
   return (
     <>
       <Flex>
         <Block right={4}>
-          <Button>Gen 1</Button>
+          <animated.span style={style}>
+            <Button onClick={() => setIsBooped(!isBooped)}>Gen 1</Button>
+          </animated.span>
         </Block>
         <Block right={4}>
           <Button>Gen 2</Button>
--- components/PokemonCard/PokemonCard.js
+++ components/PokemonCard/PokemonCard.js
@@ -8,19 +8,42 @@ import { Block, Card, Flex, H3 } from "../../primitives";
 import Styles from "./styles";
 import { isEmptyObject } from "../../utils/isEmptyUtils";
 import Tag from "../Tag";
+import { animated, useSpring } from "react-spring";
 
 const PokemonCard = ({ pokemonName, pokemonUrl }) => {
   const [pokemonData, setPokemonData] = useState({});
+  //   const [style, trigger] = useBoop(boopConfig);
+  const [isBooped, setIsBooped] = useState(false);
 
   useEffect(() => {
     const fetchPromise = fetch(pokemonUrl);
-
     fetchPromise
       .then((response) => {
         return response.json();
       })
       .then((w) => setPokemonData(w));
   }, [pokemonName, pokemonUrl]);
+
+  const x = 0;
+  const y = 10;
+  const rotation = 0;
+  const scale = 1.1;
+  const timing = 150;
+
+  const style = useSpring({
+    display: "inline-block",
+    transform: isBooped
+      ? `translate(${x}px, ${y}px)
+         rotate(${rotation}deg)
+         scale(${scale})`
+      : `translate(0px, 0px)
+         rotate(0deg)
+         scale(1)`,
+    config: {
+      tension: 300,
+      friction: 10,
+    },
+  });
 
   const {
     id = "",
@@ -34,8 +57,8 @@ const PokemonCard = ({ pokemonName, pokemonUrl }) => {
   if (isEmptyObject(pokemonData)) return <>null</>;
 
   return (
-    <>
-      <Card minHeight="200px">
+    <animated.span style={style}>
+      <Card minHeight="200px" onClick={() => setIsBooped(!isBooped)}>
         <Flex column align="center" justify="space-between" style={{ height: "100%" }}>
           <Image width="150px" height="150px" src={front_default} />
           <div>
@@ -67,7 +90,7 @@ const PokemonCard = ({ pokemonName, pokemonUrl }) => {
           </div>
         </Flex>
       </Card>
-    </>
+    </animated.span>
   );
 };
 
